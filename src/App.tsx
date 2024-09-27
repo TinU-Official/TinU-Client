17986e28dbdf2ef43645c677b2405cbaf1aa384e
jungwoo3490 <jungwoo3490@naver.com>
2024-09-28 06:27:44 +0900

    init: React-Query 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Router'
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // 쿼리가 실패했을 때 재시도하지 않도록 설정
+        retry: false,
+        // window가 focus되었을 때 자동으로 데이터를 가져오지 않도록 설정
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
 
   return (
-    <RouterProvider router={router}/>
-  )
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
