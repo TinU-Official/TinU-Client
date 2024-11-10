649376f0c3be085a1712df172b8b98bf9e3aba77
jungwoo3490 <jungwoo3490@naver.com>
2024-11-11 00:17:13 +0900

    chore: apply globalstyles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { router } from "./Router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import theme from "./styles/theme";
-import { ThemeProvider } from "@emotion/react";
+import { Global, ThemeProvider } from "@emotion/react";
+import gStyle from "./styles/GlobalStyles";
 
 function App() {
   const queryClient = new QueryClient({
@@ -20,6 +21,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
+        <Global styles={gStyle} />
         <RouterProvider router={router} />
         <ReactQueryDevtools />
       </ThemeProvider>
