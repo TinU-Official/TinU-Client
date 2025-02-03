02fa0e74875d2b1b94925113a28f477551f02aa7
조주희 <carla_cho@naver.com>
2025-02-03 22:08:27 +0900

    chore: export 수정 및 svg파일 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,9 +5,28 @@ import IcCheckGrey from "./svg/check/ic_check-grey.svg?react";
 import IcCheckMint from "./svg/check/ic_check-mint.svg?react";
 import IcCheckMintWhite from "./svg/check/ic_check-mintwhite.svg?react";
 import IcCheckWhiteGrey from "./svg/check/ic_check-whitegrey.svg?react";
+import IcCheckTransparent from "./svg/check/ic_check-transparent.svg?react";
 
 import IcStep1 from "./svg/register/ic_step1.svg?react";
+import IcStep2 from "./svg/register/ic_step2.svg?react";
+import IcStep3 from "./svg/register/ic_step3.svg?react";
+import IcStep4 from "./svg/register/ic_step4.svg?react";
 import IcGreyCircle from "./svg/register/ic_grey-circle.svg?react";
 import IcBack from "./svg/ic_back.svg?react";
+import IcNext from "./svg/ic_next.svg?react";
 
-export { IcRabbit, IcCheckGrey, IcCheckMint, IcCheckMintWhite, IcCheckWhiteGrey, IcStep1, IcBack, IcGreyCircle };
+export {
+  IcRabbit,
+  IcCheckGrey,
+  IcCheckMint,
+  IcCheckMintWhite,
+  IcCheckWhiteGrey,
+  IcCheckTransparent,
+  IcStep1,
+  IcStep2,
+  IcStep3,
+  IcStep4,
+  IcBack,
+  IcNext,
+  IcGreyCircle,
+};
--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,11 +1,9 @@
-const Register = {
+export const Register = {
   TEXT_STEP1: "간편 회원가입을 위해<br /> 약관 동의가 필요해요",
   TEXT_STEP2: " 학교 계정 인증을 위한<br /> 이메일을 입력하세요",
   DESCRIPTION_STEP2: " 최초 1회 이메일 인증이 필요해요.",
   TEXT_STEP3: "나만의 프로필을<br /> 설정하세요",
   DESCRIPTION_STEP3: "마이페이지에서 언제든 수정 가능해요.",
-  TEXT_STE43: "추가적인 정보를<br /> 입력하세요",
+  TEXT_STEP4: "추가적인 정보를<br /> 입력하세요",
   DESCRIPTION_STEP4: "추가정보를 입력하고 신뢰도를 높여보세요!",
 };
-
-export { Register };
