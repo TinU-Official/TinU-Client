5e786a6af689ab49077707f1ffe80c747f385729
jungwoo3490 <jungwoo3490@naver.com>
2025-02-12 23:00:14 +0900

    chore: tanstack query setting

---
+++ src/app/providers.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { isServer, MutationCache, QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+function makeQueryClient() {
+  return new QueryClient({
+    queryCache: new QueryCache({ onError: handleQueryError }),
+    mutationCache: new MutationCache({ onError: handleMutationError }),
+    defaultOptions: { queries: { retry: false } },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+}
+
+// 중앙 집중 에러 핸들링 방식으로 구현
+// 추후 에러 핸들링 로직 추가 예정
+
+async function handleQueryError() {}
+
+async function handleMutationError() {}
