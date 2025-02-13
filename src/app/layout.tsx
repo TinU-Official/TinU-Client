a7233d4a14c4b33004bca0ca3fbccac7c5c408f4
jungwoo3490 <jungwoo3490@naver.com>
2025-02-14 00:41:17 +0900

    feat: add themeClass in global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/global.css";
 import Providers from "./providers";
+import { themeClass } from "@/styles/theme.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${themeClass}`}>
         <Providers>{children}</Providers>
       </body>
     </html>
