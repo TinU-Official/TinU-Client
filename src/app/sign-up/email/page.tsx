08172cdb9a0ffdff3eb6f8de01df275b7f4dbcbb
조주희 <carla_cho@naver.com>
2025-02-18 21:25:49 +0900

    feat: Step2 퍼블리싱

--- src/app/sign-up/email/page.tsx
+++ src/app/sign-up/email/page.tsx
@@ -1,3 +1,19 @@
+import * as styles from "./email.css";
+import React from "react";
+
+import { MemoizedStepIcon } from "@/components/SignUp/StepIcon/StepIcon";
+import EmailForm from "@/components/SignUp/Email/EmailForm/EmailForm";
+
 export default function page() {
-  return <></>;
+  return (
+    <div className={styles.termsPageWrapper}>
+      <MemoizedStepIcon step={2} />
+      <h1 className={styles.titleWrapper}>
+        간편 회원가입을 위해 <br />
+        약관 동의가 필요해요
+      </h1>
+      <h3 className={styles.descriptionWrapper}>최초 1회 이메일 인증이 필요해요.</h3>
+      <EmailForm />
+    </div>
+  );
 }
