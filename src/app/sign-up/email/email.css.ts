4b82e1c492ee7a6e2168e6ff63d031a630e3377a
조주희 <carla_cho@naver.com>
2025-02-18 21:28:17 +0900

    chore: prettier 적용 안된 부분 처리

--- src/app/sign-up/email/email.css.ts
+++ src/app/sign-up/email/email.css.ts
@@ -2,12 +2,12 @@ import { vars } from "@/styles/theme.css";
 import { style } from "@vanilla-extract/css";
 
 export const termsPageWrapper = style({
-    display: "flex",
-    flexDirection: "column",
-  
-    width: "100%",
-    height: "100%",
-  });
+  display: "flex",
+  flexDirection: "column",
+
+  width: "100%",
+  height: "100%",
+});
 
 export const titleWrapper = style({
   ...vars.fontStyles.title2_sb_22,
@@ -17,12 +17,9 @@ export const titleWrapper = style({
 });
 
 export const descriptionWrapper = style({
-    ...vars.fontStyles.body2_m_14,
-    color: vars.color.grey_6,
-  
-    marginTop: "0.5rem",
-    paddingLeft: "1rem",
-  });
+  ...vars.fontStyles.body2_m_14,
+  color: vars.color.grey_6,
 
-
-  
+  marginTop: "0.5rem",
+  paddingLeft: "1rem",
+});
