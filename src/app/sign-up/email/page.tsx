d3a0f5cb3acf03d735d8e95230b898f87a603b14
조주희 <carla_cho@naver.com>
2025-02-20 17:25:48 +0900

    fix: 회원가입 2단계 문구로 수정

--- src/app/sign-up/email/page.tsx
+++ src/app/sign-up/email/page.tsx
@@ -8,8 +8,8 @@ export default function page() {
     <div className={styles.emailPageWrapper}>
       <MemoizedStepIcon step={2} />
       <h1 className={styles.titleWrapper}>
-        간편 회원가입을 위해 <br />
-        약관 동의가 필요해요
+        학교 계정 인증을 위한 <br />
+        이메일을 입력하세요
       </h1>
       <h2 className={styles.descriptionWrapper}>최초 1회 이메일 인증이 필요해요.</h2>
       <EmailForm />
