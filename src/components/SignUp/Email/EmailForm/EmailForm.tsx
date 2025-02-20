ac505e85ca495e166475dcb7e147e601145db15c
조주희 <carla_cho@naver.com>
2025-02-20 17:32:04 +0900

    fix: 리뷰 반영 및 className 직관적인 네이밍으로 수정

--- src/components/SignUp/Email/EmailForm/EmailForm.tsx
+++ src/components/SignUp/Email/EmailForm/EmailForm.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import * as styles from "./emailForm.css";
 import React, { useState } from "react";
@@ -9,7 +9,7 @@ function EmailForm() {
   const [email, setEmail] = useState("");
   const [isTyping, setIsTyping] = useState(false);
 
-  const isValidEmail = (email: string) => /^[a-zA-Z0-9._%+-]+@kyonggi\.ac\.kr$/.test(email);
+  const isValidEmail = /^[a-zA-Z0-9._%+-]+@kyonggi\.ac\.kr$/.test(email);
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
@@ -18,11 +18,10 @@ function EmailForm() {
     setIsTyping(true);
   };
 
-  const isError = email !== "" && !isValidEmail(email);
-  const isSubmitDisabled = !isValidEmail(email);
+  const isError = email !== "" && !isValidEmail;
 
   return (
-    <div className={styles.nextWrapper}>
+    <div className={styles.emailFieldContainer}>
       <div className={styles.emailFieldWrapper}>
         <TextField
           placeholder="이메일 주소"
@@ -32,7 +31,7 @@ function EmailForm() {
           helperText={isError ? "학교 계정이 아닌 것 같아요 :(" : ""}
         />
       </div>
-      <SubmitButton disabled={isSubmitDisabled} />
+      <SubmitButton disabled={!isValidEmail} />
     </div>
   );
 }
--- src/components/SignUp/Email/EmailForm/emailForm.css.ts
+++ src/components/SignUp/Email/EmailForm/emailForm.css.ts
@@ -1,14 +1,14 @@
 import { style } from "@vanilla-extract/css";
 
-export const emailFieldWrapper = style({
-  marginTop: "4.6rem",
-});
-
-export const nextWrapper = style({
+export const emailFieldContainer = style({
   flex: 1,
 
   display: "flex",
   flexDirection: "column",
   justifyContent: "space-between",
   gap: "2rem",
+});
+
+export const emailFieldWrapper = style({
+  marginTop: "4.6rem",
 });
