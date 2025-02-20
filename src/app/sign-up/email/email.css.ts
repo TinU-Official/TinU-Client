856d1e70282e9184b1e493b36205d20e9fb5d29a
조주희 <carla_cho@naver.com>
2025-02-20 17:23:00 +0900

    fix: className 네이밍 수정 및 h2 태그로 반영

--- src/app/sign-up/email/email.css.ts
+++ src/app/sign-up/email/email.css.ts
@@ -1,7 +1,7 @@
 import { vars } from "@/styles/theme.css";
 import { style } from "@vanilla-extract/css";
 
-export const termsPageWrapper = style({
+export const emailPageWrapper = style({
   display: "flex",
   flexDirection: "column",
 
