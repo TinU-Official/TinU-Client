dcdab3ddfa291a63814dde990c3fa9beb94c1f0d
jungwoo3490 <jungwoo3490@naver.com>
2025-03-01 15:42:51 +0900

    feat: implement second banner

--- src/components/Landing/SecondBanner/SecondBanner.tsx
+++ src/components/Landing/SecondBanner/SecondBanner.tsx
@@ -1,10 +1,17 @@
 import { forwardRef } from "react";
 import * as styles from "./secondBanner.css";
+import ImgSecondBanner from "@/assets/imgs/img_second_banner.svg";
 
 export const SecondBanner = forwardRef<HTMLDivElement>((_, ref) => {
   return (
     <div ref={ref} id="secondBanner" className={styles.secondBannerWrapper}>
-      FirstBanner
+      <div className={styles.secondBannerTitleSection}>
+        <p className={styles.secondBannerText}>
+          인증받은 학생과의 거래로 <br />
+          안심하고 거래할 수 있어요
+        </p>
+      </div>
+      <ImgSecondBanner />
     </div>
   );
 });
--- src/components/Landing/SecondBanner/secondBanner.css.ts
+++ src/components/Landing/SecondBanner/secondBanner.css.ts
@@ -1,8 +1,25 @@
 import { style } from "@vanilla-extract/css";
+import { vars } from "@/styles/theme.css";
 
 export const secondBannerWrapper = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+
   width: "100%",
   height: "100dvh",
+});
 
-  backgroundColor: "orange",
+export const secondBannerTitleSection = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+
+  margin: "14.4dvh 0 13rem 0",
+});
+
+export const secondBannerText = style({
+  ...vars.fontStyles.title2_sb_22,
+
+  color: vars.color.grey_12,
 });
