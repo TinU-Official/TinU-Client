ce48019600aa2120775e0ca61c65a67de99a4d17
jungwoo3490 <jungwoo3490@naver.com>
2025-03-02 00:12:01 +0900

    refactor: remove redundancy in IndicatorBar component

--- src/components/Landing/IndicatorBar/IndicatorBar.tsx
+++ src/components/Landing/IndicatorBar/IndicatorBar.tsx
@@ -4,32 +4,21 @@ import { vars } from "@/styles/theme.css";
 import * as styles from "./indicatorBar.css";
 
 export const IndicatorBar = ({ activeIndicator }: { activeIndicator: number }) => {
+  const indicators = [1, 2, 3];
+
   return (
     <div className={styles.indicatorBarWrapper}>
-      <motion.div
-        className={styles.indicatorWrapper}
-        animate={{
-          backgroundColor: activeIndicator === 1 ? vars.color.mint_600 : vars.color.grey_4,
-          height: activeIndicator === 1 ? "3rem" : "1.2rem",
-        }}
-        transition={{ duration: 0.3 }}
-      />
-      <motion.div
-        className={styles.indicatorWrapper}
-        animate={{
-          backgroundColor: activeIndicator === 2 ? vars.color.mint_600 : vars.color.grey_4,
-          height: activeIndicator === 2 ? "3rem" : "1.2rem",
-        }}
-        transition={{ duration: 0.3 }}
-      />
-      <motion.div
-        className={styles.indicatorWrapper}
-        animate={{
-          backgroundColor: activeIndicator === 3 ? vars.color.mint_600 : vars.color.grey_4,
-          height: activeIndicator === 3 ? "3rem" : "1.2rem",
-        }}
-        transition={{ duration: 0.3 }}
-      />
+      {indicators.map((index) => (
+        <motion.div
+          key={index}
+          className={styles.indicatorWrapper}
+          animate={{
+            backgroundColor: activeIndicator === index ? vars.color.mint_600 : vars.color.grey_4,
+            height: activeIndicator === index ? "3rem" : "1.2rem",
+          }}
+          transition={{ duration: 0.3 }}
+        />
+      ))}
     </div>
   );
 };
