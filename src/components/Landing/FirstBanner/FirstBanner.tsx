67b43aa07781f46a28b65de3331c8e6b49d3d325
jungwoo3490 <jungwoo3490@naver.com>
2025-03-01 12:54:25 +0900

    feat: implement first banner

--- src/components/Landing/FirstBanner/FirstBanner.tsx
+++ src/components/Landing/FirstBanner/FirstBanner.tsx
@@ -1,10 +1,17 @@
 import { forwardRef } from "react";
 import * as styles from "./firstBanner.css";
+import IcLogo from "@/assets/icons/ic_logo.svg";
+import Image from "next/image";
+import ImgFirstBanner from "@/assets/imgs/img_first_banner.png";
 
 export const FirstBanner = forwardRef<HTMLDivElement>((_, ref) => {
   return (
     <div ref={ref} id="firstBanner" className={styles.firstBannerWrapper}>
-      FirstBanner
+      <div className={styles.firstBannerTitleSection}>
+        <h1 className={styles.firstBannerTitle}>당신의 생활을 보다 스마트하게</h1>
+        <IcLogo />
+      </div>
+      <Image src={ImgFirstBanner} alt="First Banner Image" width={225} height={210} />
     </div>
   );
 });
--- src/components/Landing/FirstBanner/firstBanner.css.ts
+++ src/components/Landing/FirstBanner/firstBanner.css.ts
@@ -1,8 +1,27 @@
 import { style } from "@vanilla-extract/css";
+import { vars } from "@/styles/theme.css";
 
 export const firstBannerWrapper = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+
   width: "100%",
   height: "100dvh",
+});
 
-  backgroundColor: "blue",
+export const firstBannerTitleSection = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+
+  margin: "14.4dvh 0 9.6rem 0",
+});
+
+export const firstBannerTitle = style({
+  ...vars.fontStyles.title2_sb_22,
+
+  marginBottom: "2.3rem",
+
+  color: vars.color.grey_12,
 });
