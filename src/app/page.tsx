df64442e58ddbf3292bd5c61e220c0a143cbbd22
jungwoo3490 <jungwoo3490@naver.com>
2025-03-01 23:15:06 +0900

    feat: add start button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
 "use client";
 
+import * as styles from "./landing.css";
+
+import Button from "@/components/Common/Button/Button";
 import { FirstBanner } from "@/components/Landing/FirstBanner/FirstBanner";
 import { IndicatorBar } from "@/components/Landing/IndicatorBar/IndicatorBar";
 import { SecondBanner } from "@/components/Landing/SecondBanner/SecondBanner";
 import { ThirdBanner } from "@/components/Landing/ThirdBanner/ThirdBanner";
 import { useActiveIndicator } from "@/hooks/Landing/useActiveIndicator";
+import { useRouter } from "next/navigation";
 
 export default function Landing() {
+  const router = useRouter();
   const { activeIndicator, firstBannerRef, secondBannerRef, thirdBannerRef } = useActiveIndicator();
+
+  const handleClickStartButton = () => {
+    router.push("/login");
+  };
 
   return (
     <>
@@ -15,6 +24,9 @@ export default function Landing() {
       <FirstBanner ref={firstBannerRef} />
       <SecondBanner ref={secondBannerRef} />
       <ThirdBanner ref={thirdBannerRef} />
+      <div className={styles.fixedButtonWrapper}>
+        <Button onClick={handleClickStartButton}>틴유 시작하기</Button>
+      </div>
     </>
   );
 }
