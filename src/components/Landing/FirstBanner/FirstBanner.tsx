504e30ddd6d0d188d15a54885238815a52f567c6
jungwoo3490 <jungwoo3490@naver.com>
2025-03-01 16:58:40 +0900

    fix: change text logo file name

--- src/components/Landing/FirstBanner/FirstBanner.tsx
+++ src/components/Landing/FirstBanner/FirstBanner.tsx
@@ -1,6 +1,6 @@
 import { forwardRef } from "react";
 import * as styles from "./firstBanner.css";
-import IcLogo from "@/assets/icons/ic_logo.svg";
+import IcTextLogo from "@/assets/icons/ic_text_logo.svg";
 import ImgFirstBanner from "@/assets/imgs/img_first_banner.svg";
 
 export const FirstBanner = forwardRef<HTMLDivElement>((_, ref) => {
@@ -9,7 +9,7 @@ export const FirstBanner = forwardRef<HTMLDivElement>((_, ref) => {
       <div className={styles.firstBannerContent}>
         <div className={styles.firstBannerTitleSection}>
           <h1 className={styles.firstBannerTitle}>당신의 생활을 보다 스마트하게</h1>
-          <IcLogo />
+          <IcTextLogo />
         </div>
         <ImgFirstBanner />
       </div>
