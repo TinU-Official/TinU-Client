c54043de1a8e2ca24616acefbcad4d71088139c5
jungwoo3490 <jungwoo3490@naver.com>
2025-03-06 02:47:46 +0900

    feat: implement chat page header

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,21 @@
+import { Header } from "@/components/Common/Header/Header";
+import * as styles from "./chat.css";
+import { BackButton } from "@/components/Common/BackButton/BackButton";
+import { MoreButton } from "@/components/Chat/MoreButton/MoreButton";
+
+const mock = {
+  nickname: "jungwoo3490",
+};
+
+export default function Chat() {
+  return (
+    <>
+      <Header
+        isSticky
+        left={<BackButton link="chat-list" />}
+        center={<span className={styles.nickname}>{mock.nickname}</span>}
+        right={<MoreButton />}
+      />
+    </>
+  );
+}
