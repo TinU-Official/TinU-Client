9c71c92117ec8de718eeb90bb1f147645da80840
jungwoo3490 <jungwoo3490@naver.com>
2025-03-11 01:45:39 +0900

    feat: remove virtual keyboard calculating logic

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Header } from "@/components/Common/Header/Header";
 import * as styles from "./chat.css";
 import { BackButton } from "@/components/Common/BackButton/BackButton";
@@ -7,22 +5,14 @@ import { MoreButton } from "@/components/Chat/MoreButton/MoreButton";
 import { ProductInfo } from "@/components/Chat/ProductInfo/ProductInfo";
 import { ChatBottomSheet } from "@/components/Chat/ChatBottomSheet/ChatBottomSheet";
 import { ChatScreen } from "@/components/Chat/ChatScreen/ChatScreen";
-import { useKeyboardHeight } from "@/hooks/Chat/useKeyboardHeight";
 
 const mock = {
   nickname: "test",
 };
 
 export default function Chat() {
-  const keyboardHeight = useKeyboardHeight();
-
   return (
-    <div
-      className={styles.chatPageWrapper}
-      style={{
-        height: keyboardHeight > 0 ? `calc(100dvh - ${keyboardHeight}px)` : "100dvh",
-      }}
-    >
+    <div className={styles.chatPageWrapper}>
       <Header
         isSticky
         left={<BackButton link="chat-list" />}
