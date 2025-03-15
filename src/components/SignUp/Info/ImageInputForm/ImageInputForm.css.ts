a3b727a3695584ea5a63ccaf5ce5a0ce9df82aee
조주희 <carla_cho@naver.com>
2025-03-15 19:47:21 +0900

    feat: 이미지업로드 컴포넌트 퍼블리싱

---
+++ src/components/SignUp/Info/ImageInputForm/ImageInputForm.css.ts
@@ -0,0 +1,10 @@
+import { style } from "@vanilla-extract/css";
+
+export const ImageInputFormWrapper = style({
+  display: "flex",
+  flexDirection: "column",
+
+  width: "100%",
+});
+
+export const ImageInputButton = style({});
