71fb2d7cf4741569bdb18f1419e30881e40ed27e
조주희 <carla_cho@naver.com>
2025-03-15 19:47:43 +0900

    feat: 정보입력-클라이언트 컴포넌트 구현

---
+++ src/components/SignUp/Info/UserProfileForm/UserProfileForm.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, { useState } from "react";
+
+import * as styles from "./UserProfileForm.css";
+import ImageInputForm from "../ImageInputForm/ImageInputForm";
+import TextField from "@/components/Common/TextField/TextField";
+import { SubmitButton } from "../../Email/SubmitButton/SubmitButton";
+
+function UserProfileForm() {
+  const [nickname, setNickname] = useState("");
+
+  const isValidNickname = nickname.length >= 2 && nickname.length <= 15;
+  const isDuplicateNickname = false; //
+  const isAvailableNickname = isValidNickname && !isDuplicateNickname && nickname !== "";
+
+  const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    if (/[^가-힣a-zA-Z0-9]/.test(inputValue)) return;
+    setNickname(inputValue);
+  };
+
+  const isError = nickname !== "" && (!isValidNickname || isDuplicateNickname);
+  const helperText =
+    nickname === ""
+      ? ""
+      : !isValidNickname
+        ? "닉네임은 2~15자여야 해요!"
+        : isDuplicateNickname
+          ? "이미 사용중인 닉네임이에요 :("
+          : "사용 가능한 닉네임이에요";
+
+  return (
+    <div className={styles.userProfileFormWrapper}>
+      <div>
+        <ImageInputForm />
+        <div className={styles.nicknameFieldWrapper}>
+          <TextField
+            placeholder="닉네임을 입력하세요"
+            isError={isError}
+            onChange={handleNicknameChange}
+            value={nickname}
+            helperText={helperText}
+          />
+        </div>
+      </div>
+      <SubmitButton disabled={true} />
+    </div>
+  );
+}
+
+export default UserProfileForm;
