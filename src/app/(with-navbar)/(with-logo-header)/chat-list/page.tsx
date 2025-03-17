3d8ba890d4324c879e50dda92a291aec1fdaaa74
jungwoo3490 <jungwoo3490@naver.com>
2025-03-18 03:37:02 +0900

    feat: add ChatListItem component

--- src/app/(with-navbar)/(with-logo-header)/chat-list/page.tsx
+++ src/app/(with-navbar)/(with-logo-header)/chat-list/page.tsx
@@ -5,8 +5,52 @@ import * as styles from "./chatList.css";
 
 import { useState } from "react";
 import { FilterOption } from "@/types/chatListTypes";
+import { ChatListItem } from "@/components/ChatList/ChatListItem/ChatListItem";
 
 const FILTER_OPTIONS: FilterOption[] = ["전체", "판매", "구매"];
+
+const mock = [
+  {
+    id: 1,
+    productImage: "",
+    nickname: "테스트",
+    previewMessage: "안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요",
+    receiveTime: "3시간 전",
+    notReadCount: 1,
+  },
+  {
+    id: 2,
+    productImage: "",
+    nickname: "테스트",
+    previewMessage: "안녕하세요",
+    receiveTime: "3시간 전",
+    notReadCount: 0,
+  },
+  {
+    id: 3,
+    productImage: "",
+    nickname: "테스트",
+    previewMessage: "안녕하세요안녕하세요",
+    receiveTime: "3시간 전",
+    notReadCount: 0,
+  },
+  {
+    id: 4,
+    productImage: "",
+    nickname: "테스트",
+    previewMessage: "안녕하세요",
+    receiveTime: "3시간 전",
+    notReadCount: 0,
+  },
+  {
+    id: 5,
+    productImage: "",
+    nickname: "테스트",
+    previewMessage: "안녕하세요",
+    receiveTime: "3시간 전",
+    notReadCount: 0,
+  },
+];
 
 export default function page() {
   const [filterOption, setFilterOption] = useState("전체");
@@ -23,6 +67,18 @@ export default function page() {
             option={option}
             isSelected={filterOption === option}
             handleClickFilterButton={handleClickFilterButton}
+          />
+        ))}
+      </div>
+      <div>
+        {mock.map(({ id, productImage, nickname, previewMessage, receiveTime, notReadCount }) => (
+          <ChatListItem
+            key={id}
+            productImage={productImage}
+            nickname={nickname}
+            previewMessage={previewMessage}
+            receiveTime={receiveTime}
+            notReadCount={notReadCount}
           />
         ))}
       </div>
