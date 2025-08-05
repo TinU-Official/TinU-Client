1e6efbfd7191e3f955072d59cfd7fed23a6e3b67
조주희 <carla_cho@naver.com>
2025-08-05 22:56:32 +0900

    fix: SubmitButton 폴더 위치 수정 및 1개로 통일

--- src/components/Common/SubmitButton/SubmitButton.tsx
+++ src/components/Common/SubmitButton/SubmitButton.tsx
@@ -4,12 +4,13 @@ import * as styles from "./submitButton.css";
 
 interface SubmitButtonProps {
   disabled: boolean;
+  label?: string;
 }
 
-export const SubmitButton = memo(function SubmitButton({ disabled }: SubmitButtonProps) {
+export const SubmitButton = memo(function SubmitButton({ disabled, label = "다음" }: SubmitButtonProps) {
   return (
     <Button className={styles.submitButton} type="submit" disabled={disabled}>
-      <span>다음</span>
+      <span>{label}</span>
     </Button>
   );
 });
