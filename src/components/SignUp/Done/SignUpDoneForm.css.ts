e3554ad1353512e5e0c3dad6ddacea9b04961887
조주희 <carla_cho@naver.com>
2025-08-07 13:15:21 +0900

    feat: 회원가입 완료 페이지 구현

---
+++ src/components/SignUp/Done/SignUpDoneForm.css.ts
@@ -0,0 +1,42 @@
+import { style } from "@vanilla-extract/css";
+
+export const SignUpDoneFormWrapper = style({
+  flex: 1,
+
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  alignItems: "center",
+
+  marginTop: "5rem",
+});
+
+export const fixedButtonWrapper = style({
+  position: "fixed",
+  bottom: "3.4rem",
+  left: "0",
+  right: "0",
+
+  padding: "0 2rem",
+});
+
+export const celebrationWrapper = style({
+  position: "relative",
+});
+
+export const confettiBackground = style({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  zIndex: 0,
+
+  transform: "scale(1.6)",
+  transformOrigin: "center",
+});
+
+export const image = style({
+  position: "relative",
+  zIndex: 1,
+});
