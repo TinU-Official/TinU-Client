7e7b03368eb47388285ca3053e8aa6f269407ec6
조주희 <carla_cho@naver.com>
2025-08-09 23:19:20 +0900

    fix: SignUpDoneForm 제거 및 page.tsx 구조에 통합

--- src/app/sign-up/done/done.css.ts
+++ src/app/sign-up/done/done.css.ts
@@ -15,3 +15,43 @@ export const titleWrapper = style({
   marginTop: "5.2rem",
   paddingLeft: "1rem",
 });
+
+export const contentWrapper = style({
+  flex: 1,
+
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+
+  paddingBottom: "10rem",
+});
+
+export const celebrationWrapper = style({
+  position: "relative",
+});
+
+export const confettiBackground = style({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  zIndex: 0,
+
+  transform: "scale(1.5)",
+  transformOrigin: "center",
+});
+
+export const image = style({
+  position: "relative",
+  zIndex: 1,
+});
+
+export const fixedButtonWrapper = style({
+  position: "fixed",
+  bottom: "3.4rem",
+  left: "0",
+  right: "0",
+
+  padding: "0 2rem",
+});
--- src/app/sign-up/done/page.tsx
+++ src/app/sign-up/done/page.tsx
@@ -1,11 +1,24 @@
-import SignUpDoneForm from "@/components/SignUp/Done/SignUpDoneForm";
 import * as styles from "./done.css";
+import ImgSignUpDone from "@/assets/icons/ic_signup_done.svg";
+import Confetti from "@/assets/animations/Confetti";
+import Button from "@/components/Common/Button/Button";
 
 export default function Page() {
   return (
     <div className={styles.donePageWrapper}>
       <h1 className={styles.titleWrapper}>가입이 완료되었어요</h1>
-      <SignUpDoneForm />
+      <div className={styles.contentWrapper}>
+        <div className={styles.celebrationWrapper}>
+          <div className={styles.confettiBackground}>
+            <Confetti />
+          </div>
+          <ImgSignUpDone className={styles.image} />
+        </div>
+      </div>
+
+      <div className={styles.fixedButtonWrapper}>
+        <Button>틴유 시작하기</Button>
+      </div>
     </div>
   );
 }
