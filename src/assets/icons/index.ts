7d661a9474219fa227b88457ad4409eb7b4092b1
jungwoo3490 <jungwoo3490@naver.com>
2025-08-10 18:34:57 +0900

    feat: memo HOC 적용

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,73 +1,7 @@
 import IcAirplaneGrey from "./ic_airplane_grey.svg";
 import IcAirplaneMint from "./ic_airplane_mint.svg";
-import IcArrowRight from "./ic_arrow_right.svg";
 import IcCamera from "./ic_camera.svg";
-import IcChatGrey from "./ic_chat_grey.svg";
-import IcChatMint from "./ic_chat_mint.svg";
-import IcCheckComplete from "./ic_check_complete.svg";
-import IcCheckGrey from "./ic_check_grey.svg";
-import IcCheckMint from "./ic_check_mint.svg";
-import IcCheckTransparent from "./ic_check_transparent.svg";
-import IcCheckWhiteGrey from "./ic_check_whitegrey.svg";
-import IcChevronDown from "./ic_chevron_down.svg";
 import IcChevronLeft from "./ic_chevron_left.svg";
-import IcChevronRight from "./ic_chevron_right.svg";
-import IcChevronUp from "./ic_chevron_up.svg";
-import IcClose from "./ic_close.svg";
-import IcGreyCircle from "./ic_grey_circle.svg";
-import IcHamburger from "./ic_hamburger.svg";
-import IcHomeGrey from "./ic_home_grey.svg";
-import IcHomeMint from "./ic_home_mint.svg";
-import IcLogoGoogle from "./ic_logo_google.svg";
-import IcLogoKakao from "./ic_logo_kakao.svg";
-import IcLogoNaver from "./ic_logo_naver.svg";
-import IcMoreVertical from "./ic_more_vertical.svg";
 import IcPlus from "./ic_plus.svg";
-import IcProfile from "./ic_profile.svg";
-import IcSignUpDone from "./ic_signup_done.svg";
-import IcStep1 from "./ic_step1.svg";
-import IcStep2 from "./ic_step2.svg";
-import IcStep3 from "./ic_step3.svg";
-import IcStep4 from "./ic_step4.svg";
-import IcSwap from "./ic_swap.svg";
-import IcTextLogo from "./ic_text_logo.svg";
-import IcTextLogoSmall from "./ic_text_logo_small.svg";
-import IcUserGrey from "./ic_user_grey.svg";
 
-export {
-  IcAirplaneGrey,
-  IcAirplaneMint,
-  IcArrowRight,
-  IcCamera,
-  IcChatGrey,
-  IcChatMint,
-  IcCheckComplete,
-  IcCheckGrey,
-  IcCheckMint,
-  IcCheckTransparent,
-  IcCheckWhiteGrey,
-  IcChevronDown,
-  IcChevronLeft,
-  IcChevronRight,
-  IcChevronUp,
-  IcClose,
-  IcGreyCircle,
-  IcHamburger,
-  IcHomeGrey,
-  IcHomeMint,
-  IcLogoGoogle,
-  IcLogoKakao,
-  IcLogoNaver,
-  IcMoreVertical,
-  IcPlus,
-  IcProfile,
-  IcSignUpDone,
-  IcStep1,
-  IcStep2,
-  IcStep3,
-  IcStep4,
-  IcSwap,
-  IcTextLogo,
-  IcTextLogoSmall,
-  IcUserGrey,
-};
+export { IcAirplaneGrey, IcAirplaneMint, IcCamera, IcChevronLeft, IcPlus };
--- src/components/Common/IconButton/index.tsx
+++ src/components/Common/IconButton/index.tsx
@@ -1,14 +1,14 @@
-import type { ButtonHTMLAttributes, ReactElement } from "react";
+import { memo, type ButtonHTMLAttributes, type ReactElement } from "react";
 
 interface IconButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon: ReactElement<SVGElement>;
   label: string;
 }
 
-export function IconButton({ icon, label, ...props }: IconButtonProps) {
+export const IconButton = memo(function IconButton({ icon, label, ...props }: IconButtonProps) {
   return (
     <button type="button" aria-label={label} title={label} {...props}>
       {icon}
     </button>
   );
-}
+});
