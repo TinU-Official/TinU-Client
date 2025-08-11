ac793aa6a8c95b8529b52e9d6950c364aad6cead
조주희 <carla_cho@naver.com>
2025-08-11 19:11:06 +0900

    fix: HashTag 컴포넌트 불필요한 props 제거

--- src/components/Common/HashTag/HashTag.tsx
+++ src/components/Common/HashTag/HashTag.tsx
@@ -3,13 +3,8 @@ import * as styles from "./hashTag.css";
 interface HashTagProps {
   text: string;
   isActive?: boolean;
-  onClick?: () => void;
 }
 
-export function HashTag({ text, isActive = false, onClick }: HashTagProps) {
-  return (
-    <button className={styles.tag({ state: isActive ? "active" : "inactive" })} onClick={onClick}>
-      # {text}
-    </button>
-  );
+export function HashTag({ text, isActive = false }: HashTagProps) {
+  return <button className={styles.tag({ state: isActive ? "active" : "inactive" })}># {text}</button>;
 }
