fa2994279761a0481dbb985d726ef64ca993eb49
조주희 <carla_cho@naver.com>
2025-08-11 19:09:52 +0900

    fix: ProductInfoSection 컴포넌트 분리(가독성을 위한)

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -2,10 +2,10 @@ import { BackButton } from "@/components/Common/BackButton/BackButton";
 import * as styles from "./product-detail.css";
 import { Header } from "@/components/Common/Header/Header";
 import Button from "@/components/Common/Button/Button";
-import ProductSeller from "@/components/product-detail/ProductSeller";
+import ProductMetaSection from "@/components/product-detail/ProductMetaSection/ProductMetaSection";
 import { IcHamburger } from "@/assets/icons";
-import { HashTag } from "@/components/Common/HashTag/HashTag";
 import { IconButton } from "@/components/Common/IconButton";
+import ProductInfoSection from "@/components/product-detail/ProductInfoSection/ProductInfoSection";
 
 export default function Page() {
   return (
@@ -19,28 +19,8 @@ export default function Page() {
 
       <img src="/sechon.png" alt="상품 대표사진" className={styles.productMainImg} />
 
-      <ProductSeller />
-
-      <div className={styles.productInfoWrapper}>
-        <span className={styles.productCategory}>의류 {">"} 악세사리</span>
-        <section className={styles.productDetailInfo}>
-          <h1 className={styles.productTitle}>락피쉬 니트 장갑</h1>
-          <div className={styles.productMetaWrapper}>
-            <h2 className={styles.productPrice}>8000원</h2>
-            <span className={styles.uploadTime}>5분 전</span>
-          </div>
-        </section>
-        <p className={styles.productContentWrapper}>
-          세 달 전에 구매한 제품입니다. 손이 많이 안 가서 판매하게 되었어요. 보풀도 없고, 새 상품이나 다름없어요 네고
-          가능합니다~!
-        </p>
-        <section className={styles.hashtagWrapper}>
-          <HashTag text="태그 입력(최대 30개)" />
-          <HashTag text="뜨개장갑" isActive />
-          <HashTag text="락피쉬" isActive />
-          <HashTag text="장갑" isActive />
-        </section>
-      </div>
+      <ProductMetaSection />
+      <ProductInfoSection />
 
       <div className={styles.fixedButtonWrapper}>
         <Button>채팅하기</Button>
