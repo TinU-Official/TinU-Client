fe7128e80d586085acc457d8c3ac41f65ec1ca92
조주희 <carla_cho@naver.com>
2025-08-16 01:34:34 +0900

    fix: 리뷰반영-Link 컴포넌트 및 h3 태그 수정

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -3,8 +3,11 @@ import { BackButton } from "@/components/Common/BackButton/BackButton";
 import * as styles from "./settings.css";
 import { Header } from "@/components/Common/Header/Header";
 import React from "react";
+import Link from "next/link";
 
 export default function page() {
+  // TODO: Link 컴포넌트의 href 경로 정해지면 수정하기
+
   return (
     <>
       <Header
@@ -14,21 +17,21 @@ export default function page() {
         right={<IcHamburger />}
       />
       <section className={styles.settingSection}>
-        <span className={styles.settingText}>계정</span>
-        <div className={styles.infoRow}>
+        <h3 className={styles.settingText}>계정</h3>
+        <Link className={styles.infoRow} href={"/"}>
           <span className={styles.infoText}>내 정보 수정</span>
           <IcChevronRight />
-        </div>
-        <div className={styles.infoRow}>
+        </Link>
+        <Link className={styles.infoRow} href={"/"}>
           <span className={styles.infoText}>탈퇴하기</span>
           <IcChevronRight />
-        </div>
+        </Link>
       </section>
 
       <div className={styles.divider}></div>
 
       <section className={styles.settingSection}>
-        <span className={styles.settingText}>설정</span>
+        <h3 className={styles.settingText}>설정</h3>
         <div className={styles.infoRow}>
           <span className={styles.infoText}>앱 버전</span>
           <span className={styles.versionText}>1.0.52</span>
@@ -38,11 +41,11 @@ export default function page() {
       <div className={styles.divider}></div>
 
       <section className={styles.settingSection}>
-        <span className={styles.settingText}>정보</span>
-        <div className={styles.infoRow}>
+        <h3 className={styles.settingText}>정보</h3>
+        <Link className={styles.infoRow} href={"/"}>
           <span className={styles.infoText}>약관 및 정책</span>
           <IcChevronRight />
-        </div>
+        </Link>
       </section>
     </>
   );
