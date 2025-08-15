3fea57da8707dbe4270e7326a91b2f39dcec6d21
JUHEE CHO <118191954+juheehasaeyo@users.noreply.github.com>
2025-08-16 01:38:41 +0900

    feat: 설정페이지 퍼블리싱 (#113)
    * feat: 설정페이지 퍼블리싱
    
    * fix: 리뷰반영-Link 컴포넌트 및 h3 태그 수정

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,52 @@
+import { IcChevronRight, IcHamburger } from "@/assets/icons";
+import { BackButton } from "@/components/Common/BackButton/BackButton";
+import * as styles from "./settings.css";
+import { Header } from "@/components/Common/Header/Header";
+import React from "react";
+import Link from "next/link";
+
+export default function page() {
+  // TODO: Link 컴포넌트의 href 경로 정해지면 수정하기
+
+  return (
+    <>
+      <Header
+        isSticky
+        left={<BackButton />}
+        center={<span className={styles.headerText}>설정</span>}
+        right={<IcHamburger />}
+      />
+      <section className={styles.settingSection}>
+        <h3 className={styles.settingText}>계정</h3>
+        <Link className={styles.infoRow} href={"/"}>
+          <span className={styles.infoText}>내 정보 수정</span>
+          <IcChevronRight />
+        </Link>
+        <Link className={styles.infoRow} href={"/"}>
+          <span className={styles.infoText}>탈퇴하기</span>
+          <IcChevronRight />
+        </Link>
+      </section>
+
+      <div className={styles.divider}></div>
+
+      <section className={styles.settingSection}>
+        <h3 className={styles.settingText}>설정</h3>
+        <div className={styles.infoRow}>
+          <span className={styles.infoText}>앱 버전</span>
+          <span className={styles.versionText}>1.0.52</span>
+        </div>
+      </section>
+
+      <div className={styles.divider}></div>
+
+      <section className={styles.settingSection}>
+        <h3 className={styles.settingText}>정보</h3>
+        <Link className={styles.infoRow} href={"/"}>
+          <span className={styles.infoText}>약관 및 정책</span>
+          <IcChevronRight />
+        </Link>
+      </section>
+    </>
+  );
+}
---
+++ src/app/settings/settings.css.ts
@@ -0,0 +1,44 @@
+import { vars } from "@/styles/theme.css";
+import { style } from "@vanilla-extract/css";
+
+export const headerText = style({
+  ...vars.fontStyles.subtitle1_sb_16,
+  color: vars.color.grey_12,
+});
+
+export const settingSection = style({
+  width: "100%",
+
+  display: "flex",
+  flexDirection: "column",
+  gap: "2rem",
+  padding: "2rem",
+});
+
+export const infoRow = style({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
+
+export const settingText = style({
+  ...vars.fontStyles.body1_sb_14,
+  color: vars.color.grey_6,
+});
+
+export const infoText = style({
+  ...vars.fontStyles.subtitle2_m_16,
+  color: vars.color.grey_11,
+});
+
+export const divider = style({
+  width: "100%",
+  height: "1rem",
+
+  backgroundColor: vars.color.grey_2,
+});
+
+export const versionText = style({
+  ...vars.fontStyles.body3_r_14,
+  color: vars.color.grey_7,
+});
