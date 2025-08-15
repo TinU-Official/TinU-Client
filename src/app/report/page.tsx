b1669665ff5345e151721fbee214ae02a2bc3dcb
조주희 <carla_cho@naver.com>
2025-08-15 15:09:35 +0900

    feat: 신고페이지 1차 퍼블리싱

---
+++ src/app/report/page.tsx
@@ -0,0 +1,27 @@
+import { Header } from "@/components/Common/Header/Header";
+import * as styles from "./report.css";
+import { IcHamburger } from "@/assets/icons";
+import { BackButton } from "@/components/Common/BackButton/BackButton";
+import ReportReasonForm from "@/components/report/ReportReasonForm";
+
+export default function page() {
+  return (
+    <>
+      <Header
+        isSticky
+        left={<BackButton />}
+        center={<span className={styles.headerText}>신고하기</span>}
+        right={<IcHamburger />}
+      />
+      <div className={styles.reportTextWrapper}>
+        <h1 className={styles.titleWrapper}>
+          해당 사용자로 인해 <br />
+          어떤 문제가 발생했나요?
+        </h1>
+        <h2 className={styles.descriptionWrapper}>회원님의 신고는 익명으로 처리됩니다.</h2>
+      </div>
+
+      <ReportReasonForm />
+    </>
+  );
+}
