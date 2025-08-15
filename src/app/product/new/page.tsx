03120b109c94ed6f7e2e91bb9086cf3b994673cb
jungwoo3490 <jungwoo3490@naver.com>
2025-08-16 00:30:56 +0900

    feat: Common component들 추가

--- src/app/product/new/page.tsx
+++ src/app/product/new/page.tsx
@@ -1,10 +1,33 @@
+"use client";
+
 import { IcClose } from "@/assets/icons";
+import { HashTag } from "@/components/Common/HashTag/HashTag";
 import { Header } from "@/components/Common/Header/Header";
+import Select from "@/components/Common/Select/Select";
+import TextField from "@/components/Common/TextField/TextField";
+import * as styles from "./product-new.css";
 
 export default function page() {
   return (
     <>
       <Header center="판매하기" right={<IcClose />} isSticky />
+      <main className={styles.mainWrapper}>
+        <TextField placeholder="제목" />
+        <TextField placeholder="가격" rightAddOn={<span className={styles.won}>원</span>} />
+        <Select placeholder="카테고리 선택하기">
+          <Select.Trigger />
+          <Select.Main>
+            <Select.Option value="사과">사과</Select.Option>
+            <Select.Option value="귤">귤</Select.Option>
+            <Select.Option value="포도">포도</Select.Option>
+            <Select.Option value="망고">망고</Select.Option>
+            <Select.Option value="수박">수박</Select.Option>
+          </Select.Main>
+        </Select>
+        <textarea placeholder="내용을 입력하세요" className={styles.textarea} />
+
+        <HashTag text="태그 입력(최대 30개)" />
+      </main>
     </>
   );
 }
--- src/app/product/new/product-new.css.ts
+++ src/app/product/new/product-new.css.ts
@@ -5,3 +5,29 @@ export const headerTitle = style({
   ...vars.fontStyles.subtitle1_sb_16,
   color: vars.color.grey_12,
 });
+
+export const mainWrapper = style({
+  width: "100%",
+  padding: "0 2rem",
+});
+
+export const won = style({
+  ...vars.fontStyles.subtitle2_m_16,
+  color: vars.color.grey_10,
+});
+
+export const textarea = style({
+  width: "100%",
+  height: "14rem",
+
+  ...vars.fontStyles.subtitle3_r_16,
+  backgroundColor: vars.color.grey_2,
+  border: `1px solid ${vars.color.grey_4}`,
+  borderRadius: "1rem",
+
+  padding: "1.5rem",
+  marginTop: "1rem",
+
+  resize: "none",
+  outline: "none",
+});
