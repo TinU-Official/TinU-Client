2935d53ebe98eec7fa4a4d4de0cd2df3344f0bce
jungwoo3490 <jungwoo3490@naver.com>
2025-08-16 00:26:55 +0900

    fix: flexbox 외부에서도 너비 깨지지 않도록 width fit content 추가

--- src/components/Common/HashTag/hashTag.css.ts
+++ src/components/Common/HashTag/hashTag.css.ts
@@ -3,6 +3,7 @@ import { recipe } from "@vanilla-extract/recipes";
 
 export const tag = recipe({
   base: {
+    width: "fit-content",
     borderRadius: "1.8rem",
     padding: "0.8rem 1rem",
     border: "1px solid",
