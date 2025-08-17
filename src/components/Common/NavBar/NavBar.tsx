d80f584eb52f1f9c356f3ade976b164785468ce3
조주희 <carla_cho@naver.com>
2025-08-17 11:21:53 +0900

    style: NavBar 수정된 디자인 반영

--- src/components/Common/NavBar/NavBar.tsx
+++ src/components/Common/NavBar/NavBar.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { IcChatGrey, IcChatMint, IcHomeGrey, IcHomeMint, IcUserGrey } from "@/assets/icons";
+import {
+  IcChatActive,
+  IcChatInactive,
+  IcHomeInactive,
+  IcHomeActive,
+  IcMypageActive,
+  IcMypageInactive,
+} from "@/assets/icons";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import * as styles from "./navBar.css";
 
 const navItems = [
-  { href: "/home", label: "홈", icon: <IcHomeGrey />, activeIcon: <IcHomeMint /> },
-  { href: "/chat-list", label: "채팅", icon: <IcChatGrey />, activeIcon: <IcChatMint /> },
-  // TODO: 마이페이지 activeIcon 문의
-  { href: "/mypage", label: "마이페이지", icon: <IcUserGrey />, activeIcon: <IcUserGrey /> },
+  { href: "/home", label: "홈", icon: <IcHomeInactive />, activeIcon: <IcHomeActive /> },
+  { href: "/chat-list", label: "채팅", icon: <IcChatInactive />, activeIcon: <IcChatActive /> },
+  { href: "/mypage", label: "마이", icon: <IcMypageInactive />, activeIcon: <IcMypageActive /> },
 ];
 
 export function NavBar() {
@@ -17,13 +23,16 @@ export function NavBar() {
 
   return (
     <nav className={styles.navBarWrapper}>
-      {navItems.map(({ href, label, icon, activeIcon }) => (
-        <Link key={href} href={href} className={styles.navItemWrapper}>
-          {pathname === href && <div className={styles.activeBar} />}
-          {pathname === href ? activeIcon : icon}
-          <span className={styles.navItemLabel}>{label}</span>
-        </Link>
-      ))}
+      {navItems.map(({ href, label, icon, activeIcon }) => {
+        const isActive = pathname === href;
+
+        return (
+          <Link key={href} href={href} className={styles.navItemWrapper}>
+            {isActive ? activeIcon : icon}
+            <span className={styles.navItemLabel({ isActive })}>{label}</span>
+          </Link>
+        );
+      })}
     </nav>
   );
 }
