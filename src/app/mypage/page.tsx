d2907007f119418a29b96f76582644194752681a
조주희 <carla_cho@naver.com>
2025-08-22 18:24:26 +0900

    fix: 리뷰 반영

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -24,19 +24,19 @@ export default function page() {
             <Image src="/sechon.png" alt="내 프로필 사진" className={styles.profileImg} fill />
             <IconButton icon={<IcPencil />} label="프로필 사진 수정 버튼" className={styles.pencil} />
           </div>
-          <span className={styles.nickname}>세숑</span>
+          <h2 className={styles.nickname}>세숑</h2>
         </section>
 
         <section className={styles.myInfoContainer}>
           <div className={styles.myInfoWrapper}>
             <div className={styles.myInfoBox}>
-              <h2 className={styles.infoText1}>학교명</h2>
+              <span className={styles.infoText1}>학교명</span>
               <h3 className={styles.infoText2}>경기대학교</h3>
             </div>
             <IcVerticalLine className={styles.verticalLine} />
 
             <div className={styles.myInfoBox}>
-              <h2 className={styles.infoText1}>학년</h2>
+              <span className={styles.infoText1}>학년</span>
               <h3 className={styles.infoText2}>4학년</h3>
             </div>
           </div>
@@ -44,29 +44,31 @@ export default function page() {
 
         <div className={styles.divider}></div>
 
-        <span className={styles.myactivityText}>나의 활동</span>
-        {/*TODO: [나의 활동] 3가지 각각 클릭 시 페이지 이동 예정 */}
+        <div className={styles.myActivityWrapper}>
+          <span className={styles.myActivityText}>나의 활동</span>
+          {/*TODO: [나의 활동] 3가지 각각 클릭 시 페이지 이동 예정 */}
 
-        <section className={styles.myActivitySection}>
-          <button className={styles.activityButton.forSale}>
-            <span className={styles.activityText}>판매중</span>
-            <div className={styles.iconWrapper}>
-              <IcForSale />
-            </div>
-          </button>
-          <button className={styles.activityButton.soldout}>
-            <span className={styles.activityText}>거래완료</span>
-            <div className={styles.iconWrapper}>
-              <IcSoldout />
-            </div>
-          </button>
-          <button className={styles.activityButton.hidden}>
-            <span className={styles.activityText}>숨김</span>
-            <div className={styles.iconWrapper}>
-              <IcHidden />
-            </div>
-          </button>
-        </section>
+          <section className={styles.myActivitySection}>
+            <button className={styles.activityButton.forSale}>
+              <span className={styles.activityText}>판매중</span>
+              <div className={styles.iconWrapper}>
+                <IcForSale />
+              </div>
+            </button>
+            <button className={styles.activityButton.soldOut}>
+              <span className={styles.activityText}>거래완료</span>
+              <div className={styles.iconWrapper}>
+                <IcSoldout />
+              </div>
+            </button>
+            <button className={styles.activityButton.hidden}>
+              <span className={styles.activityText}>숨김</span>
+              <div className={styles.iconWrapper}>
+                <IcHidden />
+              </div>
+            </button>
+          </section>
+        </div>
       </div>
       <NavBar />
     </>
