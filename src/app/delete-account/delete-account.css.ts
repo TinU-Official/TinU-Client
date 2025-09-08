380f1a5112e70ed2c7aa23ecc6562b0bc841122d
조주희 <carla_cho@naver.com>
2025-09-08 22:53:41 +0900

    feat: 탈퇴하기 페이지 퍼블리싱

---
+++ src/app/delete-account/delete-account.css.ts
@@ -0,0 +1,76 @@
+import { vars } from "@/styles/theme.css";
+import { style } from "@vanilla-extract/css";
+
+export const withdrawTextWrapper = style({
+  width: "100%",
+  height: "100%",
+
+  padding: "3rem",
+});
+
+export const headerText = style({
+  ...vars.fontStyles.subtitle1_sb_16,
+  color: vars.color.grey_12,
+});
+
+export const titleWrapper = style({
+  ...vars.fontStyles.title2_sb_22,
+});
+
+export const descriptionWrapper = style({
+  ...vars.fontStyles.body2_m_14,
+  color: vars.color.grey_6,
+
+  marginTop: "0.5rem",
+});
+
+export const withdrawInfoWrapper = style({
+  padding: "0 3rem",
+});
+
+export const withdrawInfo = style({
+  display: "flex",
+  alignItems: "center",
+  gap: "1rem",
+
+  margin: "2rem 0",
+});
+
+export const withdrawInfoText = style({
+  ...vars.fontStyles.body2_m_14,
+  color: vars.color.grey_10,
+});
+
+export const verticalLine = style({
+  marginLeft: "2rem",
+  height: "7rem",
+});
+
+export const withdrawDescription = style({
+  display: "flex",
+  alignItems: "center",
+  gap: "1rem",
+});
+
+export const withdrawDescriptionText = style({
+  ...vars.fontStyles.label1_m_12,
+  color: vars.color.grey_6,
+});
+
+export const descriptionFlex = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "2rem",
+});
+
+export const fixedButtonWrapper = style({
+  display: "flex",
+  gap: "1.2rem",
+
+  position: "fixed",
+  bottom: "3.4rem",
+  left: "0",
+  right: "0",
+
+  padding: "0 2rem",
+});
