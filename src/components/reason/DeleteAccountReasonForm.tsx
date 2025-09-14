e36e7ff4ea355e41cbf4b0ad10eafe6fc7bf876d
조주희 <carla_cho@naver.com>
2025-09-14 12:16:40 +0900

    fix: 리뷰 반영- 탈퇴사유/신고사유 컴포넌트 외부로 분리

--- src/components/reason/DeleteAccountReasonForm.tsx
+++ src/components/reason/DeleteAccountReasonForm.tsx
@@ -5,15 +5,15 @@ import Select from "../Common/Select/Select";
 import * as styles from "./deleteAccountReasonForm.css";
 import Button from "../Common/Button/Button";
 
-function DeleteAccountReasonForm() {
-  const 탈퇴사유 = {
-    noProduct: "사고 싶은 물품이 없어요",
-    notSell: "물품이 안 팔려요",
-    noMannerUser: "비매너 사용자를 만났어요",
-    newAccount: "새 계정을 만들고 싶어요",
-    elseReason: "기타 사유 (직접 입력)",
-  } as const;
+const 탈퇴사유 = {
+  noProduct: "사고 싶은 물품이 없어요",
+  notSell: "물품이 안 팔려요",
+  noMannerUser: "비매너 사용자를 만났어요",
+  newAccount: "새 계정을 만들고 싶어요",
+  elseReason: "기타 사유 (직접 입력)",
+} as const;
 
+function DeleteAccountReasonForm() {
   const [reason, setReason] = useState<(typeof 탈퇴사유)[keyof typeof 탈퇴사유] | null>(null);
   const [guitarReason, setGuitarReason] = useState("");
 
--- src/components/report/ReportReasonForm.tsx
+++ src/components/report/ReportReasonForm.tsx
@@ -5,15 +5,15 @@ import Select from "../Common/Select/Select";
 import * as styles from "./reportReasonForm.css";
 import Button from "../Common/Button/Button";
 
-function ReportReasonForm() {
-  const 신고사유 = {
-    mismatchInfo: "상품 정보가 실제와 달라요",
-    noShow: "거래 약속을 지키지 않았어요",
-    abusiveUser: "욕설이나 비하 발언을 했어요",
-    suspicious: "사기가 의심돼요",
-    elseReason: "기타 사유 (직접 입력)",
-  } as const;
+const 신고사유 = {
+  mismatchInfo: "상품 정보가 실제와 달라요",
+  noShow: "거래 약속을 지키지 않았어요",
+  abusiveUser: "욕설이나 비하 발언을 했어요",
+  suspicious: "사기가 의심돼요",
+  elseReason: "기타 사유 (직접 입력)",
+} as const;
 
+function ReportReasonForm() {
   const [reason, setReason] = useState<(typeof 신고사유)[keyof typeof 신고사유] | null>(null);
   const [guitarReason, setGuitarReason] = useState("");
 
