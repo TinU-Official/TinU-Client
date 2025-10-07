7c79a110f41034ab2aea591991683fbaa3ae590d
조주희 <carla_cho@naver.com>
2025-10-07 18:53:05 +0900

    feat: 이미지 업로드 컴포넌트 미리보기 구현

--- src/components/SignUp/Info/ImageInputForm/ImageInputForm.css.ts
+++ src/components/SignUp/Info/ImageInputForm/ImageInputForm.css.ts
@@ -1,8 +1,37 @@
+import { vars } from "@/styles/theme.css";
 import { style } from "@vanilla-extract/css";
 
 export const ImageInputFormWrapper = style({
   display: "flex",
   flexDirection: "column",
+  alignItems: "center",
+});
 
+export const previewWrapper = style({
+  width: "11rem",
+  height: "11rem",
+  borderRadius: "50%",
+  boxShadow: `inset 0 0 0 1.28px ${vars.color.grey_5}`,
+
+  position: "relative",
+  cursor: "pointer",
+});
+
+export const previewImage = style({
   width: "100%",
+  height: "100%",
+  borderRadius: "50%",
+
+  objectFit: "cover",
+});
+
+export const profileIcon = style({
+  width: "100%",
+  height: "100%",
+});
+
+export const addButton = style({
+  position: "absolute",
+  bottom: "0",
+  right: "0",
 });
--- src/components/SignUp/Info/ImageInputForm/ImageInputForm.tsx
+++ src/components/SignUp/Info/ImageInputForm/ImageInputForm.tsx
@@ -1,13 +1,54 @@
 "use client";
 
-import { IcProfile } from "@/assets/icons";
+import { IcPlusMint, IcProfile } from "@/assets/icons";
+import Image from "next/image";
 import { IconButton } from "@/components/Common/IconButton";
 import * as styles from "./ImageInputForm.css";
+import { useState, ChangeEvent } from "react";
 
 function ImageInputForm() {
+  const [image, setImage] = useState<File | null>(null);
+  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+
+  const handleClickImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    setImage(file);
+    setPreviewUrl(URL.createObjectURL(file));
+  };
+
+  const handleClickImage = () => {
+    document.getElementById("profile-image-input")?.click();
+  };
+
   return (
     <div className={styles.ImageInputFormWrapper}>
-      <IconButton icon={<IcProfile />} label="프로필 사진 변경" />
+      <input
+        id="profile-image-input"
+        type="file"
+        accept="image/*"
+        onChange={handleClickImageUpload}
+        style={{ display: "none" }}
+      />
+
+      <div className={styles.previewWrapper} onClick={handleClickImage}>
+        {previewUrl ? (
+          <Image src={previewUrl} alt="미리보기" width={11} height={11} className={styles.previewImage} />
+        ) : (
+          <IcProfile className={styles.profileIcon} />
+        )}
+
+        <IconButton
+          icon={<IcPlusMint />}
+          label="프로필 사진 등록"
+          onClick={(e) => {
+            e.stopPropagation();
+            handleClickImage();
+          }}
+          className={styles.addButton}
+        />
+      </div>
     </div>
   );
 }
