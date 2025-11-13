43151bd44a5e18b47d840770811c35f929ea0165
jungwoo3490 <jungwoo3490@naver.com>
2025-11-13 22:11:00 +0900

    feat: 토큰 관리 위치 세션 스토리지로 변경

--- src/common/apis/stores/token-store.ts
+++ src/common/apis/stores/token-store.ts
@@ -3,16 +3,16 @@ const SESSION_STORAGE_KEY = "ACCESS_TOKEN";
 export const accessTokenStore = {
   get: () => {
     if (typeof window === "undefined") return null;
-    return localStorage.getItem(SESSION_STORAGE_KEY);
+    return sessionStorage.getItem(SESSION_STORAGE_KEY);
   },
   set: (atk: string) => {
     if (typeof window !== "undefined") {
-      localStorage.setItem(SESSION_STORAGE_KEY, atk);
+      sessionStorage.setItem(SESSION_STORAGE_KEY, atk);
     }
   },
   clear: () => {
     if (typeof window !== "undefined") {
-      localStorage.removeItem(SESSION_STORAGE_KEY);
+      sessionStorage.removeItem(SESSION_STORAGE_KEY);
     }
   },
 };
