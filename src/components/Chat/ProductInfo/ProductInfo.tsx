5fba3f3e1e835072ca9856dc183710b40661f89e
조주희 <carla_cho@naver.com>
2026-02-14 01:50:02 +0900

    fix: 뱃지 컴포넌트 적용 및 ProductInfo 조건문 처리

--- src/components/Chat/ProductInfo/ProductInfo.tsx
+++ src/components/Chat/ProductInfo/ProductInfo.tsx
@@ -3,15 +3,20 @@ import * as styles from "./productInfo.css";
 import ImgMock from "@/assets/imgs/img_mock.png";
 
 import { memo } from "react";
+import Badge from "@/components/Common/Badge/Badge";
 
-export const ProductInfo = memo(function ProductInfo() {
+interface ProductInfoProps {
+  status?: "forSale" | "soldOut";
+}
+
+export const ProductInfo = memo(function ProductInfo({ status }: ProductInfoProps) {
   return (
     <div className={styles.ProductInfoWrapper}>
       <Image src={ImgMock} alt="상품 이미지" width={50} height={50} />
       <div className={styles.ProductDetailContainer}>
         <div className={styles.ProductTitleAndStatus}>
           <p className={styles.ProductTitle}>라탄 자전거 바구니</p>
-          <div className={styles.ProductStatus}>거래중</div>
+          {status && <Badge status={status}>{status === "forSale" ? "거래중" : "판매완료"}</Badge>}
         </div>
         <p className={styles.ProductPrice}>6,000원</p>
       </div>
