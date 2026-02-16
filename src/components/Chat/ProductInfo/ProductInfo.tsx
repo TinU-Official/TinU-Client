64c5ab2c699727bc73b1c102677b51335b3898ff
조주희 <carla_cho@naver.com>
2026-02-16 17:16:44 +0900

    fix: 리뷰 반영

--- src/components/Chat/ProductInfo/ProductInfo.tsx
+++ src/components/Chat/ProductInfo/ProductInfo.tsx
@@ -3,20 +3,20 @@ import * as styles from "./productInfo.css";
 import ImgMock from "@/assets/imgs/img_mock.png";
 
 import { memo } from "react";
-import Badge from "@/components/Common/Badge/Badge";
+import SalesBadge from "@/components/Common/Badge/SalesBadge";
 
 interface ProductInfoProps {
-  status?: "forSale" | "soldOut";
+  isSoldOut?: boolean;
 }
 
-export const ProductInfo = memo(function ProductInfo({ status }: ProductInfoProps) {
+export const ProductInfo = memo(function ProductInfo({ isSoldOut }: ProductInfoProps) {
   return (
     <div className={styles.ProductInfoWrapper}>
       <Image src={ImgMock} alt="상품 이미지" width={50} height={50} />
       <div className={styles.ProductDetailContainer}>
         <div className={styles.ProductTitleAndStatus}>
           <p className={styles.ProductTitle}>라탄 자전거 바구니</p>
-          {status && <Badge status={status}>{status === "forSale" ? "거래중" : "판매완료"}</Badge>}
+          {<SalesBadge isSoldOut={isSoldOut} />}
         </div>
         <p className={styles.ProductPrice}>6,000원</p>
       </div>
