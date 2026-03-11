d624e6d0d76258c1f83c614f6c73bec1ed5a8642
jungwoo3490 <jungwoo3490@naver.com>
2026-03-11 20:52:41 +0900

    feat: select 공통 컴포넌트 구현

--- apps/mobile/src/assets/icons/index.ts
+++ apps/mobile/src/assets/icons/index.ts
@@ -1,4 +1,6 @@
 export { default as IcCamera } from "./ic_camera.svg";
+export { default as IcChevronDown } from "./ic_chevron_down.svg";
 export { default as IcChevronLeft } from "./ic_chevron_left.svg";
+export { default as IcChevronUp } from "./ic_chevron_up.svg";
 export { default as IcMoreVertical } from "./ic_more_vertical.svg";
 export { default as IcSwap } from "./ic_swap.svg";
